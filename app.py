c1a560108652afdc1e7b11116fb588df8f4e94d8
Swayam-28 <your-email@example.com>
2026-04-07 20:45:28 +0530

    Fix port for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -87,4 +88,5 @@ def status():
 
 # ----- Run -----
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
